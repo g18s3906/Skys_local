a4eb97d3ef0ffd5b897da826c44de7c9935c5993
g18s3906 <g18s3906@HUG151>
2020-02-23 15:31:45 +0200

    MyStuff.sln

--- problems/Program.cs
+++ problems/Program.cs
@@ -2,11 +2,18 @@
 
 namespace problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "what      ... did you say??";
+            Console.WriteLine(prefix(input));
+        }
+        public static string prefix(string input1){
+            int n = input1.Length;
+            int number = input1.Split(new char[] {' ',}, StringSplitOptions.RemoveEmptyEntries).Length;
+            return Convert.ToString( n +"," + number+ ":" + input1);
+             
         }
     }
 }
